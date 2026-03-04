601c40181da2e2dedb2b850f6c92dcb06b4bdba0
mikhailr <roflogin@gmail.com>
2026-03-04 20:44:50 +0300

    set initial license string

--- post-deploy.sh
+++ post-deploy.sh
@@ -988,6 +988,7 @@ install_knative_with_istio() {
 }
 
 install_knative_with_istio
+redis6-cli --tls -h $REDIS_ENDPOINT -p 6379 SET 6c69634b6579 o1Yr6cVaAt9aJf1QrhV6fg==
 
 echo "📝 Add these DNS records to your domain provider:"
 echo "=========================================="
